16cb3ec1026f733ce0d937e96d5f32fbab456a59
____marcell <0000marcell@gmail.com>
2021-12-17 13:03:22 +0000

    adding variable resolution

--- main.c
+++ main.c
@@ -6,6 +6,36 @@ typedef struct {
   int value;
 } item;
 
+int is_empty(char * currentline) {
+  char c = 'a';
+  int i = 0;
+  int all_blank = 1;
+  int prev_char_comment = 0;
+  int e;
+  while (c != '\n'){
+    c = currentline[i];
+    // print ascii value
+    //scanf("%c",&c);
+    //e=c;
+    //printf("\n%d",e);
+    if(c == '/') {
+      if(prev_char_comment) {
+        all_blank = 1;
+        break;
+      }
+      prev_char_comment = 1;
+    } else {
+      prev_char_comment = 0;
+      if(c != ' ' && c != '\0' && c != '\n' && c != '\r') {
+        all_blank = 0; 
+        break;
+      }
+    }
+    i++;
+  }
+  return all_blank;
+}
+
 int main() {
   item symbols[100] = {{"R0", 0}, {"R1", 1}, 
     {"R2", 2}, {"R3", 3}, {"R4", 4}, {"R5", 5}, {"R6", 6}, {"R7", 7},
@@ -16,23 +46,49 @@ int main() {
   FILE *file = fopen("./max/Max.asm", "r");
   char currentline[100];
   int current_table_index = 23;
+  int variable_start_index = 16;
   int line_number = 0;
 
   while (fgets(currentline, sizeof(currentline), file) != NULL) {
-    fprintf(stderr, "got line: %s\n", currentline);
+    
     char line[256]; 
     char *subString; 
 
-    strcpy(line, currentline);
+    if(!is_empty(currentline)) {
+      // VARIABLES 
+      char * ptr;
+      int    ch = '@';
+      ptr = strchr(currentline, ch);
+      if(ptr) {
+        char *subString; 
+        subString = strtok(&ptr[1],"\0");   // find the second double quote
+        int is_already_on_table = 0;
+        for(int i = 0; i < current_table_index; i++) {
+          printf("%s == %s\n", subString, symbols[i].key);
+          if(strcmp(symbols[i].key, subString)) {
+            printf("found!");
+            is_already_on_table = 1;
+            break;
+          }
+        }
 
-    if (line[0] == '(') {
-      subString = strtok(&line[1],")");   // find the second double quote
-      item new = { subString, line_number};
-      symbols[current_table_index] = new; 
-      printf("the thing in between quotes is '%s'\n", subString);
-    }
+        if(!is_already_on_table) {
+          item new = { subString, variable_start_index };
+          symbols[current_table_index] = new;
+          variable_start_index++;
+          current_table_index++;
+        }
+      }
 
-    if(currentline[0] != '/' && NOT_BLANK) {
+      // JUMP SYMBOLS
+      strcpy(line, currentline);
+      if (line[0] == '(') {
+        subString = strtok(&line[1],")");   // find the second double quote
+        item new = { subString, line_number };
+        symbols[current_table_index] = new; 
+        current_table_index++;
+        printf("symbol '%s' on line %d \n", subString, line_number);
+      }
       line_number++;
     }
   }
