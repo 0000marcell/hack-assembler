2e0d80afe3142f6ddf7aadffb7d110e4d67fc84a
____marcell <0000marcell@gmail.com>
2022-01-03 21:45:07 +0000

    checking if address is already a number

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+int isNumber(char s[]) {
+  for(int i = 0; s[i]!= '\0'; i++) {
+    if(isdigit(s[i]) == 0)
+      return 0;
+  }
+  return 1;
+}
 
 typedef struct {
   char *key;
@@ -48,52 +57,56 @@ int main() {
   int current_table_index = 23;
   int variable_start_index = 16;
   int line_number = 0;
+  char *file_lines[256];
 
+  // JUMP SYMBOLS
   while (fgets(currentline, sizeof(currentline), file) != NULL) {
     if(!is_empty(currentline)) {
-      // JUMP SYMBOLS
+      file_lines[line_number] = strdup(currentline);
       if (currentline[0] == '(') {
         char line[256]; 
         strcpy(line, currentline);
-        printf("%s\n", line);
         char *subString;
         subString = strtok(&line[1],")");   // find the second double quote
+        // check if subString is a number, if it's it doesn't need to be added
         symbols[current_table_index].key = strdup(subString);
         symbols[current_table_index].value = line_number;
         current_table_index++;
       }
-      char * ptr;
-      int    ch = '@';
-      //ptr = strchr(currentline, ch);
-      //if(ptr) {
-        //char *subString = strtok(&ptr[1], "\r"); 
-        //printf("%c %d", subString[2], subString[2]);
-        //int is_already_on_table = 0;
-        //for(int i = 0; i < current_table_index; i++) {
-          //printf("%s == %s\n", subString, symbols[i].key);
-          //printf(">> %d\n", strcmp(symbols[i].key, subString));
-          //if(strcmp(symbols[i].key, subString) == 0) {
-            //printf("found!\n");
-            //is_already_on_table = 1;
-            //break;
-          //}
-        //}
-
-        //if(!is_already_on_table) {
-          //printf("subString: %s vsi: %d\n", subString, variable_start_index);
-          //item n = { subString, variable_start_index };
-          //symbols[current_table_index] = n;
-          //variable_start_index++;
-          //current_table_index++;
-        //}
-      //}
       line_number++;
     }
   }
 
-  /* printf("k: %s v: %d\n", symbols[23].key, symbols[23].value); */
-  /* printf("k: %s v: %d\n", symbols[24].key, symbols[24].value); */
-  /* printf("k: %s v: %d\n", symbols[25].key, symbols[25].value); */
+  // VARIABLES
+  for(int i = 0; i < line_number; i++) {
+    char * ptr;
+    int    ch = '@';
+    ptr = strchr(file_lines[i], ch);
+    if(ptr) {
+      char *subString = strtok(&ptr[1], "\r"); 
+      subString = strtok(subString, "\n"); 
+      subString = strtok(subString, " "); 
+      //printf("%c %d", subString[2], subString[2]);
+      int is_already_on_table = 0;
+      for(int i = 0; i < current_table_index; i++) {
+        //printf("%s == %s\n", subString, symbols[i].key); 
+        //printf(">> %d\n", strcmp(symbols[i].key, subString)); 
+        if(strcmp(symbols[i].key, subString) == 0) {
+          //printf("found!\n");
+          is_already_on_table = 1;
+          break;
+        }
+      }
+
+      if(!is_already_on_table && !isNumber(subString)) {
+        //printf("subString: %s vsi: %d\n", subString, variable_start_index);
+        symbols[current_table_index].key = strdup(subString);
+        symbols[current_table_index].value = variable_start_index;
+        variable_start_index++;
+        current_table_index++;
+      }
+    }
+  }
 
   for(int i = 0; i < current_table_index; i++) {
     printf("k: %s v: %d\n", symbols[i].key, symbols[i].value);
